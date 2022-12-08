3c55ea832bf255f89db9f4559bd8cc366da0f478
eandras <eandras@chemaxon.com>
2022-12-08 11:38:19 +0100

    Solution for Day 08

--- src/day-08/solve.ts
+++ src/day-08/solve.ts
@@ -5,12 +5,52 @@ import math from '../utils/mathUtils.js'
 import { readFromFolder } from '../utils/fileUtils.js';
 import { patternMatch } from '../utils/stringutils.js';
 
+function isVisible(tree: number, direction:number[][]) {
+  return direction.some(col => col.every(c => c < tree))
+}
+
+function scenicScore(tree: number, direction:number[][]) {
+  const scores = direction.map(d => {
+    let score = 0
+    for (let i = 0; i < d.length; i += 1) {
+      score += 1
+      if (d[i] > tree || d[i] === tree) {
+        break
+      }
+    }
+    return score
+  })
+
+  return scores.reduce((a, b) => a * b)
+}
+
 export default function solve() {
   const dirname = path.dirname(fileURLToPath(import.meta.url));
-  const content = readFromFolder(dirname, 'example.txt')
+  const content = readFromFolder(dirname, 'input.txt')
 
-  const result = content.split('\n');
+  const forest = content.split('\n').map(s => [...s].map(Number));
+  const width = forest[0].length;
+  const height = forest.length;
 
-  console.log('PART I')
-  console.log('PART II')
+  let innerVisible = 0
+  const scenicScores = []
+
+  for (let x = 1; x < width - 1; x += 1) {
+    for (let y = 1; y < height - 1; y += 1) {
+      const directions = [
+        _.flatten(math.column(forest, y)).slice(0, x).reverse(), // up
+        _.flatten(math.column(forest, y)).slice(x + 1), // down
+        forest[x].slice(0, y).reverse(), // left
+        forest[x].slice(y + 1), // right
+      ];
+      const currentTree = forest[x][y];
+
+      innerVisible += isVisible(currentTree, directions) ? 1 : 0
+      scenicScores.push(scenicScore(currentTree, directions))
+    }
+  }
+  const visibleTreesFromTheEdge = 2 * width + 2 * (height - 2)
+  const visible = visibleTreesFromTheEdge + innerVisible;
+  console.log('PART I', visible)
+  console.log('PART II', _.max(scenicScores))
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import { autoDownload } from './utils/fileUtils.js';
 import { leadingZeroDay } from './utils/stringutils.js';
 
+// Start execution measurement
+console.time('Script execution')
 // Read day from arguments
 const day = process.argv[2] || new Date().getDate();
-
 console.log('\x1b[33m%s\x1b[0m', `\n 🎄 Day ${leadingZeroDay(day)}`); // cyan
 
 // Auto-download the input for the selected day
 await autoDownload(day);
 
-// Import the solving function for that day
-const solve = await import(`./day-${leadingZeroDay(day)}/solve.js`);
+// Import and execute the solving function for that day
+(await import(`./day-${leadingZeroDay(day)}/solve.js`)).default();
 
-// Execute the default export
-solve.default()
 console.log('\x1b[32m%s\x1b[0m', '----------------------------------------------------------')
+// Stop execution measurement
+console.timeEnd('Script execution')
