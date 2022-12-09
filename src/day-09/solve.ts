6cdf3c9fb02eb988dec251414383e5d99e06d8a3
eandras <eandras@chemaxon.com>
2022-12-09 13:41:03 +0100

    Solution for Day 09

---
+++ src/day-09/solve.ts
@@ -0,0 +1,73 @@
+/* eslint-disable no-param-reassign */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import _ from 'lodash';
+import math from '../utils/mathUtils.js'
+import { readFromFolder } from '../utils/fileUtils.js';
+import { patternMatch } from '../utils/stringutils.js';
+
+type Position = {
+  x: number
+  y: number
+}
+
+type Knot = {
+  pos : Position
+  history: Position[]
+}
+
+function moveHead(H: Knot, direction: string) {
+  switch (direction) {
+    case 'R': H.pos.x += 1; return;
+    case 'U': H.pos.y += 1; return;
+    case 'L': H.pos.x -= 1; return;
+    case 'D': H.pos.y -= 1;
+  }
+  H.history.push({ ...H.pos })
+}
+
+function moveTail(T: Knot, H: Knot) {
+  const xDiff = H.pos.x - T.pos.x;
+  const yDiff = H.pos.y - T.pos.y;
+
+  // They are in the same row and they are too far away from each other
+  if (T.pos.y === H.pos.y && (xDiff > 1 || xDiff < -1)) {
+    T.pos.x += 1 * Math.sign(xDiff)
+    // Same column
+  } else if (T.pos.x === H.pos.x && (yDiff > 1 || yDiff < -1)) {
+    T.pos.y += 1 * Math.sign(yDiff)
+    // Diagonal
+  } else if (xDiff > 1 || xDiff < -1 || yDiff > 1 || yDiff < -1) {
+    T.pos.x += 1 * Math.sign(xDiff)
+    T.pos.y += 1 * Math.sign(yDiff)
+  }
+  T.history.push({ ...T.pos })
+}
+
+function uniquePositions(ropeEnd:Knot) {
+  const uniqPos = _.uniq(ropeEnd.history.map(c => `${c.x}-${c.y}`))
+  return uniqPos.length
+}
+
+export default function solve() {
+  const dirname = path.dirname(fileURLToPath(import.meta.url));
+  const content = readFromFolder(dirname, 'input.txt')
+
+  const moves = content.split('\n').map(c => patternMatch(c, '$str $int'))
+
+  // Initialize
+  const ropeLength = 10;
+  const rope:Knot[] = Array(ropeLength).fill('')
+    .map(() => ({ pos: { x: 0, y: 0 }, history: [{ x: 0, y: 0 }] }))
+
+  moves.forEach(move => {
+    const [direction, times] = move
+    for (let i = 0; i < times; i += 1) {
+      moveHead(rope[0], direction)
+      for (let j = 1; j < rope.length; j += 1) {
+        moveTail(rope[j], rope[j - 1])
+      }
+    }
+  });
+  console.log('RESULT', uniquePositions(rope[ropeLength - 1]))
+}
